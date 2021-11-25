64e4bbb44e8686d7218c9dce8df52df0047302f2
dualz0 <1026453304@qq.com>
2021-11-25 16:14:25 +0800

    change material

---
+++ Source/Rendering/Rendering.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Rendering.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Rendering, "Rendering" );
